50d382cc11eb1c9291cea65c3892b26a8d9b8367
AgungTriatna <105510687+AgungTriatna@users.noreply.github.com>
2023-05-15 23:00:19 +0700

    setting middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin.api' => \App\Http\Middleware\AdminMiddleware::class, //tambahkan ini 
+        'user.api' => \App\Http\Middleware\UserMiddleware::class, //tambahkan ini 
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,35 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 
+/*
+admin routes  
+routes untuk admin, dimana terdapat middleware admin dan juga prefix awalan url "admin"
+*/
+
+Route::middleware(['admin.api'])->prefix('admin')->group(function () {
+
+    // controller dan function belum di buat semua ya
+    // kasih commet kalau sudah
+    Route::post('register', [AdminController::class, 'register']);
+    Route::get('register', [AdminController::class, 'show_register']);
+    Route::get('register/{id}', [AdminController::class, 'show_register_by_id']);
+    Route::put('register/{id}', [AdminController::class, 'update_register']);
+    Route::delete('register/{id}', [AdminController::class, 'delete_register']);
+});
+
+
+
+
+/*
+user routes  
+routes untuk admin, dimana terdapat middleware admin dan juga prefix awalan url "user"
+*/
+Route::middleware(['user.api'])->prefix('user')->group(function () {
+    //
+
+});
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
