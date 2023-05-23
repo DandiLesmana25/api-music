5989b9e7239e52f23b10fa7640bef7bb3cbb292c
AgungTriatna <105510687+AgungTriatna@users.noreply.github.com>
2023-05-23 23:27:43 +0700

    update

--- database/migrations/2023_05_18_100051_create_detail_playlist_table.php
+++ database/migrations/2023_05_18_100051_create_detail_playlist_table.php
@@ -16,11 +16,10 @@ return new class extends Migration
         Schema::create('detail_playlist', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('playlists_id');
-            $table->unsignedBigInteger('song_id');            
+            $table->unsignedBigInteger('song_id');
             $table->foreign('playlists_id')->references('id')->on('playlists')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('song_id')->references('id')->on('songs')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
-            
         });
     }
 
